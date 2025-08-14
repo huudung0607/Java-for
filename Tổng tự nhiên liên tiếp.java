807d761f66f363608841c9552f8118367b9e4918
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-14 17:26:02 +0700

    Create Tổng tự nhiên liên tiếp.java

---
+++ Tổng tự nhiên liên tiếp.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class JavaApplication1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        System.out.println(1L * n * (n + 1)/2);
+    }
+}
