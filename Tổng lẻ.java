91c8ba6d559850a09fc7d2826346d9f33f634796
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-16 12:28:31 +0700

    Create Tổng lẻ.java

---
+++ Tổng lẻ.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long sum = 0;
+        for(int i = 1; i <= 2*n - 1 ; i+=2){
+            sum += 1l * i;
+        }
+        System.out.println(sum);
+    }
+}
