ac035eae053259a83e173f71734fdce11b3bb91d
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-16 18:02:39 +0700

    Create Giai thừa.java

---
+++ Giai thừa.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long giaiThua = 1;
+        for(int i = 1;i <= n;i++){
+            giaiThua *= 1l * i;
+        }
+        System.out.println(giaiThua);
+    }
+}
