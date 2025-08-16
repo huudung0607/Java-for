7aacce454b9a6a7b90fe9cfb9b8eba5f87c4ec24
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-16 19:37:52 +0700

    Create Tam giác cân số.java

---
+++ Tam giác cân số.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+         int cnt = 0;
+         for(int i = 1;i <= n; i++){
+             for(int j = 1; j <= n - i ; j++){
+                 System.out.print(" " + " ");
+             }
+             for(int j = i ; j <= 2 * i - 1;j++){
+                 System.out.print(j + " ");
+             }
+             for(int j = 2 * i - 2; j >= i ;j--){
+                 System.out.print(j + " ");
+             }
+             System.out.println();
+         }
+    }
+}
