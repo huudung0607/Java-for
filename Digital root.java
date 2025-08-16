230e534aabcfd28fbfb6caff02d56f55f94232a4
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-16 19:19:42 +0700

    Create Digital root.java

---
+++ Digital root.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         long n = sc.nextLong();
+         System.out.println(1 + (n - 1) % 9);
+    }
+}
