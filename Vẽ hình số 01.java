98a52272fb59edd1907602dbef8797b21ef94db8
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-17 10:32:07 +0700

    Create Vẽ hình số 01.java

---
+++ Vẽ hình số 01.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n;i++){
+            for(int j =1 ; j<= n;j++){
+                if((i + j) % 2 == 0){
+                    System.out.print(0);
+                }
+                else System.out.print(1);
+            }
+            System.out.println();
+        }
+    }
+}
