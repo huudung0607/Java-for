53238874700b3836ab3c5c93cf6b659567db44ce
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-18 17:46:26 +0700

    Create  Số hoàn hảo bản dễ.java

---
+++ Số hoàn hảo bản dễ.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 1;i < n; i++){
+            if(n % i == 0){
+                sum += i;
+            }
+        }
+        if(sum == n) System.out.print("28tech");
+        else System.out.print("29tech");
+    }
+}
